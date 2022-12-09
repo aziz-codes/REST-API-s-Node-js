e1dbfcc25fb3e9776a1b1bbe65f1ea7ef65b3d56
aziz-codes <azizisoffline@gmail.com>
2022-12-09 23:48:37 +0500

    conneced to mongo db cluster

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+const connectDB = require("./db/connect");
 
 const PORT = process.env.PORT || 5000;
 const products_routes = require("./routes/products");
@@ -9,9 +11,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", products_routes);
-
 const start = async () => {
   try {
+    await connectDB(process.env.MONGODB_URL);
     app.listen(PORT, () => {
       console.log(`${PORT} You have connected`);
     });
