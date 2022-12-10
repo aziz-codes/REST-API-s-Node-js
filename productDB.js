45278efacba3fe92bbfd9bcc73fe0c7fd35f92f4
aziz-codes <azizisoffline@gmail.com>
2022-12-10 19:08:10 +0500

    done with sending collections to live cluster

---
+++ productDB.js
@@ -0,0 +1,17 @@
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const Product = require("./models/product");
+const ProductJson = require("./products.json");
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+
+    await Product.create(ProductJson);
+    console.log("creation succssed");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
